9e7815472759186af7987bb7d11f9bf29c0c1e56
gabibolzane <gabib@DESKTOP-SJJSNEE>
2025-11-05 18:37:23 -0300

    modificação

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -4,9 +4,14 @@ namespace MauiAppHotel.Views
 {
     public partial class ContratacaoHospedagem : ContentPage
     {
+        App PropriedadesApp;
         public ContratacaoHospedagem()
         {
             InitializeComponent();
+            PropriedadesApp = (App)Application.Current;
+
+            pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+            dtpck_checkin.MinimumDate = DateTime.Now;
         }
 
         private async void btnAvancar_Clicked(object sender, EventArgs e)
