a07a28b120a223176ff0422104af30298747bdeb
gabibolzane <gabib@DESKTOP-SJJSNEE>
2025-11-05 18:47:45 -0300

    lk

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -11,7 +11,13 @@ namespace MauiAppHotel.Views
             PropriedadesApp = (App)Application.Current;
 
             pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
             dtpck_checkin.MinimumDate = DateTime.Now;
+            dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+            dtpck_checkout.MinimumDate = dtpck_checkin.Date.Value.AddDays(1);
+            dtpck_checkout.MaximumDate = dtpck_checkin.Date.Value.AddMonths(6);
+
         }
 
         private async void btnAvancar_Clicked(object sender, EventArgs e)
